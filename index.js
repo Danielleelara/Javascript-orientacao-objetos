8b9385e0f373537fd768cdab348c2bda32d049a9
Danielle Souza <danielle.andrade@meta.com.br>
2021-11-26 14:20:52 -0300

    feat: criacao de metodos

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ class Cliente {
 
 class ContaCorrente{
     agencia;
-    saldo;
+    //#saldo = 0;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor){
+        if(valor<= 0)
+            return;
+        else
+            this._saldo += valor; 
+    }
 }
 
 const cliente1 = new Cliente();
 
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const danielle = new ContaCorrente();
+danielle.agencia = 1010;
+danielle._saldo = 1000 ;
+danielle.sacar(400);
 
 const cliente2 = new Cliente();
 
@@ -20,21 +36,14 @@ cliente2.cpf = 11122233344459;
 
 const contaCorrenteRicardo = new ContaCorrente();
 
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
-console.log(contaCorrenteRicardo.saldo)
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado)
 
-contaCorrenteRicardo.saldo = 1000;
-console.log(contaCorrenteRicardo.saldo)
+console.log(contaCorrenteRicardo);
 
-let valorSacado = 200;
-if(contaCorrenteRicardo.saldo >= valorSacado){
-    contaCorrenteRicardo.saldo -= valorSacado;
-}
-
-
-console.log(contaCorrenteRicardo.saldo)
-
-console.log(cliente2)
 
