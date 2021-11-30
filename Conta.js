9031bee24f149308e98f06e94304751b124e4602
Danielle Souza <danielle.andrade@meta.com.br>
2021-11-30 15:43:01 -0300

    feat: criando novas contas

--- Conta.js
+++ Conta.js
@@ -1,15 +1,31 @@
 export class Conta {
     constructor(saldoInicial, cliente, agencia){
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente");
+        }
+       
         this._saldo = saldoInicial;
         this._cliente = cliente;
-        this._agencia = agencia;
-       
+        this._agencia = agencia;       
     }
 
-    sacar(valor){
+    setCliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
 
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+//Método abstrato que serve apenas para ser subscrito
+
+    sacar(valor){
+       throw new Error("Você deve criar o método sacar!")
     }
 
     _sacar(valor, taxa){
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 
 const cliente1 = new Cliente('Ricardo', 111122233344);
 
+
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001 );
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
 
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 101 );
-contaPoupanca.sacar(10);
 
-console.log(contaPoupanca)
-console.log(contaCorrenteRicardo)
