a321c6bcc3b3b199e683cb0ede2d0cd6da4ff0d6
Danielle Souza <danielle.andrade@meta.com.br>
2021-12-02 10:51:31 -0300

    feat: criando sistema de funcionarios

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
+const diretor = new Diretor('Rodrigo', 10000, 12345678912);
+const gerente = new Gerente('Danielle', 5000, 78945661237);
 
-const cliente1 = new Cliente('Ricardo', 111122233344);
+const estaLogado = SistemaAutenticacao.login(diretor, '123123123');
 
-
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001 );
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
-
-
-
+console.log(estaLogado);
