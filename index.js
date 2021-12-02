34f6769c4efbfbadd0e4fa6554c9cc42b4979f49
Danielle Souza <danielle.andrade@meta.com.br>
2021-12-02 11:52:00 -0300

    feat: autenticacao

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import { Diretor } from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor('Rodrigo', 10000, 12345678912);
+diretor.cadastrarSenha('123');
 const gerente = new Gerente('Danielle', 5000, 78945661237);
+gerente.cadastrarSenha('456')
 
-const estaLogado = SistemaAutenticacao.login(diretor, '123123123');
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '456');
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123');
+const cliente = new Cliente ('Lais', 789456136, '456');
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456');
 
-console.log(estaLogado);
+
+console.log(clienteEstaLogado);
+console.log(gerenteEstaLogado);
+console.log(diretorEstaLogado);
